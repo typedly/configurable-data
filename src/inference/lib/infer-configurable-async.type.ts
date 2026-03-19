ee9e1be3e1be37de896eac21776ca09a70556106
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-03-19 13:54:16 +0000

    refactor: bring back `InferConfigurableAsync` and `InferConfigurableValue` for backward compatibility

---
+++ src/inference/lib/infer-configurable-async.type.ts
@@ -0,0 +1,16 @@
+// Interface.
+import type { InferAsync } from "@typedly/data";
+import type { ConfigurableDataShape } from "../../lib";
+/**
+ * @deprecated This utility is redundant and should not be used. In favor of just using `InferAsync` from `@typedly/data`.
+ * @description Infers the async flag from the configuration or adapter.
+ * @export
+ * @template [T=undefined] The type of the configuration object.
+ * @template [F=false] The default async flag if it cannot be inferred from `T`.
+ */
+export type InferConfigurableAsync<
+  T = undefined,
+  F = false
+> = T extends ConfigurableDataShape<any, any, infer R extends boolean>
+  ? R
+  : InferAsync<T, F> ;
