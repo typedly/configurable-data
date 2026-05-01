c6aba6cae3be1537dcab7000a33aaa2f7fc05bfb
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-05-01 14:46:39 +0000

    refactor(`ConfigurableDataShape`): remove default type for T in ConfigurableDataShape interface

--- src/lib/configurable.data.shape.ts
+++ src/lib/configurable.data.shape.ts
@@ -8,13 +8,13 @@ import type { DataConfig } from "@typedly/data";
  * @export
  * @interface ConfigurableDataShape
  * @template {DataSettings<R> | undefined} C The settings to configure the data shape.
- * @template [T=any] The value type.
+ * @template T The value type.
  * @template {boolean} [R=InferConfigurableAsync<C>] The async flag inferred from the settings.
  * @extends {DataShape<T, R>} The base data shape with value type `T` and async flag `R`.
  * @extends {Configurable<DataConfig<C, R>>} The configurable interface with data configuration `C` and async flag `R`.
  */
 export interface ConfigurableDataShape<
   C extends DataSettings<R> | undefined,
-  T = any,
+  T,
   R extends boolean = InferAsync<C>
 > extends DataShape<T, R>, Configurable<DataConfig<C, R>> {}
